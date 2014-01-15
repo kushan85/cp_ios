1b31706800d601440efc97b8e18a2cdec88be83b
Kushan Shah <kshah@zynga.com>
2014-01-15 14:23:10 -0800

    Initial Commit

---
+++ tipcalculator/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  tipcalculator
+//
+//  Created by Kushan Shah on 1/15/14.
+//  Copyright (c) 2014 Kushan Shah. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
